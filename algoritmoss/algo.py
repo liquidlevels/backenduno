b85af91e14521b25b42b88a5bc194f1aba4823fa
liquid <liquid>
2024-08-30 05:45:56 -0700

    ya tenemos con que, solo falta como

--- algoritmoss/algo.py
+++ algoritmoss/algo.py
@@ -17,37 +17,34 @@ def posicion_actual(vertical, horizontal):
     #print(f'valor: {algo[vertical][horizontal]}, vertical: {vertical}, horizontal {horizontal}')
     return [algo[vertical][horizontal], vertical, horizontal]
 
-def mas_barato(posiciones_validas):
-    for x in range(len(posiciones_validas) -1):
-        mayor = max(posiciones_validas)
-        posiciones_validas.remove(mayor)
-
-    return posiciones_validas[0]
+def mas_barato(posicionesValidas):
+    copia = posicionesValidas
+    for x in range(len(posicionesValidas) -1):
+        print(f'opciones: {posicionesValidas}')
+        mayor = max(posicionesValidas)
+        posicionesValidas.remove(mayor)
+    return posicionesValidas[0]
 
 #me da los posibles caminos que puedo tomar basado en el "safe area" 0-12 en xy
 caminos = []
 def caminos_viables(vertical, horizontal):
     #izquierda
     if posicion_actual(vertical, horizontal - 1)[2] < 0:
-        print('mevalevergacompa')
         pass
     else:
         caminos.append([algo[vertical][horizontal - 1], vertical, horizontal - 1])
     #derecha
     if posicion_actual(vertical, horizontal + 1)[2] > 12:
-        print('mevalevergacompa')
         pass
     else:
         caminos.append([algo[vertical][horizontal], vertical, horizontal + 1])
     #arriba
     if posicion_actual(vertical - 1, horizontal)[1] < 0:
-        print('mevalevergacompa')
         pass
     else:
         caminos.append([algo[vertical - 1][horizontal], vertical - 1, horizontal])
     #abajo
     if posicion_actual(vertical + 1, horizontal)[1] > 12:
-        print('mevalevergacompa')
         pass
     else:
         caminos.append([algo[vertical + 1][horizontal], vertical + 1, horizontal])
@@ -55,7 +52,11 @@ def caminos_viables(vertical, horizontal):
     return caminos
 
 def prueba():
-    print(f'don baraton: {mas_barato(caminos_viables(2,1))}')
-    print(caminos_viables(2,1))
+    baraton = mas_barato(caminos_viables(2,1))
+    print(f'el mas barato: {baraton}')
+    newVertical = baraton[1]
+    newHorizontal = baraton[2]
+    print(newVertical)
+    print(newHorizontal)
 
 prueba()
