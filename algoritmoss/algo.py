c9d34ef1141681cbe45cac52472594fb25fff4b6
liquid <liquid>
2024-09-01 18:34:53 -0700

    debug chafa y acaba cuando entre las posibles posiciones, esta la posicionFinal

--- algoritmoss/algo.py
+++ algoritmoss/algo.py
@@ -18,22 +18,38 @@ matrizHorizontal = 0
 #almacena arrays (1-4) con la informacion de los posibles caminos [valor, vertical, horizontal]
 caminos = []
 
-def camino_mas_barato(posicionesValidas):
+def camino_mas_barato(posicionesValidas, posicionFinal):
+    print(posicionesValidas)
     for y in range(len(posicionesValidas) -1):
-        if isinstance(posicionesValidas[y][0], str):
+        print('entre al for')
+        if isinstance(posicionesValidas[y][0], str) and posicionesValidas[y][0] == algo[posicionFinal[0]][posicionFinal[1]]:
+            print(f'llegamos al final {posicionesValidas[y][0]} = {posicionFinal}')
+            return
+        elif isinstance(posicionesValidas[y][0], str):
             print(f'adios, {posicionesValidas[y][0]}')
+            print(f'posicionesValidas[y]?? {posicionesValidas[y]}')
             posicionesValidas.remove(posicionesValidas[y])
-
+            print(f'posiciones validas despues: {posicionesValidas}')
+        else:
+            print('ninguna de las anteriores')
+    
+    print(f'posiciones validas antes del segundo for: {posicionesValidas}')
     for x in range(len(posicionesValidas) -1):
-        print(f'opciones: {posicionesValidas}')
+        print('entre al segundo for?')
         mayor = max(posicionesValidas)
         posicionesValidas.remove(mayor)
+        print(f'segundo for: {posicionesValidas}')
 
+    print(len(posicionesValidas))
+    if(len(posicionesValidas) > 1):
+        print(f'varios casos, elige el mejor: {posicionesValidas}')
+        return posicionesValidas
+    
+    print('llegue al final?')
     return posicionesValidas[0]
 
 #me da los posibles caminos que puedo tomar basado en el "safe area" 0-12 en xy
 def posibles_rutas(vertical, horizontal):
-    notString = isinstance(algo[vertical][horizontal],str) 
     #izquierda
     if horizontal - 1 >= 0:
         caminos.append([algo[vertical][horizontal - 1], vertical, horizontal - 1])
@@ -54,22 +70,23 @@ def el_camino_asi_es(coordenadaInicial,coordenadaFinal):
     print(f'coordenadaFinal: {coordenadaFinal}')
 
     posicionActual = algo[coordenadaInicial[0]][coordenadaInicial[1]]
-    posicionDeseada = algo[coordenadaFinal[0]][coordenadaFinal[1]]
+    posicionFinal = algo[coordenadaFinal[0]][coordenadaFinal[1]]
     
-    caminoBarato = camino_mas_barato(posibles_rutas(coordenadaInicial[0],coordenadaInicial[1]))
+    caminoBarato = camino_mas_barato(posibles_rutas(coordenadaInicial[0],coordenadaInicial[1]), posicionFinal)
     print(f'el mas barato: {caminoBarato}')
     newVertical = caminoBarato[1]
     newHorizontal = caminoBarato[2]
     print(f'nueva posicion: {newVertical},{newHorizontal}')
-    caminoBarato = []
    
-    if posicionActual == posicionDeseada:
+    if posicionActual == posicionFinal:
         print('si es igual')
         return
     else:
         print('aun no')    
-        el_camino_asi_es([newVertical,newHorizontal], [coordenadaFinal[0], coordenadaFinal[1]])
+        return el_camino_asi_es([newVertical,newHorizontal], [coordenadaFinal[0], coordenadaFinal[1]])
 
-#camino_mas_barato([[-3, 2, 2], [-3, 2, 1], [2, 2, 3], ['I', 1, 2], [3, 3, 2]])
+#camino_mas_barato([[-3, 2, 2], [-3, 2, 1], [2, 2, 3], ['I', 1, 2], [3, 3, 2]],[11,6])
 
-el_camino_asi_es([1,2], [11,6])
+camino_mas_barato([[1,11,4],['F',11,6],[0,10,5],[2,12,5]],[11,6])
+
+#el_camino_asi_es([1,2], [11,6])
